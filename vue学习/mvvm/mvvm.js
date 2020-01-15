2f8f61ba0f7cf146f8656455b8a667d74270200e
朱两边 <5630082+zacharyweb@user.noreply.gitee.com>
2020-01-15 12:35:23 +0800

    fix

--- vue学习/mvvm/mvvm.js
+++ vue学习/mvvm/mvvm.js
@@ -1,3 +1,4 @@
+// 数据劫持
 class Observer{
     constructor(data){
         this.observer(data);
@@ -10,25 +11,27 @@ class Observer{
            this.defineReactive(data,key,data[key]);
            this.observer(data[key]);
        })
-
     }
     defineReactive(data,key,value){
+        let dep = new Dep();
         Object.defineProperty(data,key,{
             get(){
+              Dep.target && dep.addSub(Dep.target);
               return value;
             },
             set:(newVal)=>{
                 if(value != newVal){
                    this.observer(newVal);
                    value = newVal; 
+                   dep.notify();
                 }
             }
         })
 
     }
-
 }
 
+// 模板编译
 class Complier{
     constructor(el,vm){
         if(this.isElementNode(el)){
@@ -68,8 +71,9 @@ class Complier{
            let {key,value,name} = attr;
            if(this.isDirective(name)){
                let expr = value;
-               let [,type] = name.split('-');
-               complierUtils[type](node,this.vm,expr);
+               let [,directive] = name.split('-'); // v-model这些
+               let [directiveName,eventName] = directive.split(':'); // 解析v-on:click这类的
+               complierUtils[directiveName](node,this.vm,expr,eventName);
            }
        })
     }
@@ -79,8 +83,6 @@ class Complier{
         if(expr.match(reg)){
              complierUtils['text'](node,this.vm,expr);
         };
-       
-      
     }
     isElementNode(node){
         return node.nodeType === 1;
@@ -90,27 +92,40 @@ class Complier{
     }
 }
 
-
-class MVVM {
-    constructor(opts){
-       this.$el = opts.el;
-       this.$data = opts.data;
-       if(this.$el){
-           new Observer(this.$data);
-           new Complier(this.$el,this);
-       }
-    }
-}
-
 const complierUtils = {
      model(node,vm,expr){
-        node.value = expr;
+    
+        new Watcher(vm,expr,(newVal)=>{
+            this.updater.modelUpdater(node,newVal); 
+        })
+        node.addEventListener('input',(e)=>{
+            let value = e.target.value;
+            this.setVal(vm,expr,value);
+        })
         this.updater.modelUpdater(node,this.getVal(vm,expr));
+     },
+     on(node,vm,expr,eventName){
+         node.addEventListener(eventName,(e)=>{
+             vm[expr].call(vm,e);
+         });
+         
+     },
+     html(node,vm,expr){
+        this.updater.htmlUpdater(node,this.getVal(vm,expr));
+        new Watcher(vm,expr,(newVal)=>{
+            this.updater.htmlUpdater(node,newVal); 
+        })
      },
      text(node,vm,expr){
         let value = this.getTextVal(node,vm,expr);
+        expr.replace(/{{(.+?)}}/g,(...args)=>{
+            new Watcher(vm,args[1],()=>{
+                this.updater.textUpdater(node,this.getTextVal(node,vm,expr)); 
+            })
+        })
         this.updater.textUpdater(node,value); 
      },
+
      getTextVal(node,vm,expr){
          return expr.replace(/{{(.+?)}}/g,(...args)=>{
              return this.getVal(vm,args[1]);
@@ -122,13 +137,114 @@ const complierUtils = {
              return result[key]
          },vm.$data)
      },
+     setVal(vm,expr,value){
+        let keyArr = expr.split('.');
+        return keyArr.reduce((result,key,index)=>{
+           if(index == keyArr.length -1){
+               return result[key] = value;
+           }
+           return result[key];
+        },vm.$data)
+     },
      updater:{
          modelUpdater(node,value){
-             console.log(value);
              node.value = value;
          },
          textUpdater(node,value){
             node.textContent = value;
+         },
+         htmlUpdater(node,value){
+            
+             node.innerHTML = value;
+
          }
      }
 }
+
+class Watcher{
+    constructor(vm,expr,cb){
+        this.vm = vm;
+        this.expr = expr;
+        this.cb = cb;
+        this.value = this.get();
+    }
+    get(){
+        Dep.target = this;
+        let value = this.getVal(this.vm,this.expr);
+        Dep.target = null;
+        return value;
+    }
+    getVal(vm,expr){
+      let value = complierUtils.getVal(vm,expr);
+      return value;
+    }
+
+    update(){
+        let newVal = this.getVal(this.vm,this.expr);
+        let oldVal = this.value;
+        if(newVal != oldVal){
+            this.cb(newVal);
+        }
+    }
+}
+
+class Dep{
+    constructor(){
+        this.subs = [];
+    }
+    addSub(watcher){
+        this.subs.push(watcher)
+    }
+    notify(){
+        this.subs.forEach((watcher)=>{
+            watcher.update();
+        })
+    }
+}
+
+
+class MVVM {
+    constructor(opts){
+       this.$el = opts.el;
+       this.$data = opts.data;
+       let computed = opts.computed;
+       let methods = opts.methods;
+
+       if(this.$el){
+           new Observer(this.$data);
+
+            for(let key in computed){
+               Object.defineProperty(this.$data,key,{
+                   get:()=>{
+                      return computed[key].call(this)
+                   }
+               })
+            };
+
+            for(let key in methods){
+                Object.defineProperty(this,key,{
+                    get:()=>{
+                       return methods[key];
+                    }
+                })
+            };
+
+           this.proxyData(this.$data);
+           new Complier(this.$el,this);
+       }
+    }
+    proxyData(data){
+        Object.keys(data).forEach((key)=>{
+            Object.defineProperty(this,key,{
+                get(){
+                    return data[key];
+                },
+                set(newVal){
+                    data[key] = newVal;
+                }
+            })
+        })
+
+    }
+}
+
